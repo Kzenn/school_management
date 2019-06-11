64908699c2e9c5d19c3519f3e117d262f425d016
Kzenn <cfrancois189@gmail.com>
2019-06-11 12:49:57 +0200

    Test & XMLtest

---
+++ XMLDAOFactory.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+/**
+ *
+ * @author Famille Francois
+ */
+public class XMLDAOFactory extends AbstractDAOFactory {
+  public DAO getClasseDAO() {      
+    return null;
+  }
+
+  public DAO getEleveDAO() {
+    return null;
+  }
+
+  public DAO getMatiereDAO() {
+    return null;
+  }
+
+  public DAO getProfesseurDAO() {
+    return null;
+  }
+}
